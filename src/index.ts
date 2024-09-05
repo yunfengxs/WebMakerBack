5264a9373947aa27ae09d9b4bed490b1238846f8
yunfengxs <1575987058@qq.com>
2024-09-06 02:38:16 +0800

    ok

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,21 @@ import bodyParser from 'body-parser';
 //import userRoutes from './routes/users';
 import userRoutes from './routes/users_router';
 import user_mulRoutes from './routes/users_mul_router';
+import father_routes from './routes/father_router';
 
 const app = express();
 const port = 8090;
+app.use((req, res, next) => {
+	//console.log('CORS headers added for request:', req);
+	res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');  // 设置允许的来源
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');  // 设置允许的方法
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // 设置允许的头部
+  res.setHeader('Access-Control-Allow-Credentials', 'true');  // 如果需要支持cookie，可以开启
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);  // 对于 OPTIONS 预检请求，返回 200 响应
+  }
+  next();
+});
 
 // 使用 bodyParser 解析 JSON 请求
 app.use(bodyParser.json());
@@ -13,6 +25,8 @@ app.use(bodyParser.json());
 // 使用用户路由
 app.use('/users', userRoutes);
 app.use('/users_mul', user_mulRoutes);
+app.use('/father', father_routes);
+
 
 // 启动服务器
 app.listen(port, () => {
